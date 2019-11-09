728b43d250d6cb67f127fc47acc1d6ab37911a3e
Raissi-Dhia <estraissi@gmail.com>
2019-11-09 09:39:32 +0100

    last warmUp

--- warmUp18.js
+++ warmUp18.js
@@ -10,3 +10,24 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+// we are going to create an empty array and than itterate over the  array of the value that we inputed and each time check if the 
+// index of the element equals to -1 which is missing we push 1 to the new arr else if it's not -1 that means that the value has been 
+// found before so we add the 1 to the initial value which is one and keep the variable inside the itteration at the end we should join the array to get a string of the result
+	function count(string){
+	  
+	  var array = string.split('') ;
+	  
+	       var curr = []
+	  for ( var i = 0; i < array.length; i++){
+	  		var initial = 1 ;
+	       if (curr.indexOf(array[i]) === -1 ) {
+	         curr.push(initial) 
+	       } else if(curr.indexOf(array[i]) !== -1){
+
+	          curr.push(initial + 1)
+	     
+	         }
+	   
+	     return curr.join('');
+	}
+}
